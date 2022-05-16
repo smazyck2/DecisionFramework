2a8f8faa3b6a48ed2e5a8eef071e274dec2e955d
Scotty <smazyck2@yahoo.com>
2022-05-16 20:57:33 +0000

    alternative / column resizing

--- eval_script.js
+++ eval_script.js
@@ -1,10 +1,25 @@
 'use strict'
 
+var resizeSample = 10
+
 const resizeObserver = new ResizeObserver(entries => {
+  if (resizeSample - 1 == 0) {
+    resizeSample = 10
+    console.log(resizeSample)
+    return
+  }
+
   entries.forEach(entry => {
+    // console.log(entry)
+
     let column = entry.target.dataset.column
-    $qAll($('evalGrid'), `[data-column${column}]`).forEach(col => {
-      // console.log(col)
+    console.log(column + ' resizing')
+
+    let columnWidth = entry.contentRect.width
+    // console.log(columnWidth)
+
+    $qAll($('evalGrid'), `[data-column=${column}]`).forEach(col => {
+      col.style.width = columnWidth + 'px'
     })
     console.log(column)
   })
